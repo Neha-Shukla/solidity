804d134047f7b70bd7573372d0205b1c594718bb
neha <gmail.com>
2020-01-26 19:37:15 +0530

    first commit

---
+++ myContract.sol
@@ -0,0 +1,17 @@
+pragma solidity ^0.5.11;
+contract SendMoneyExample {
+uint public balanceReceived;
+function receiveMoney() public payable {
+balanceReceived += msg.value;
+}
+function getBalance() public view returns(uint) {
+return address(this).balance;
+}
+function withdrawMoney() public {
+address payable to = msg.sender;
+to.transfer(this.getBalance());
+}
+function withdrawMoneyTo(address payable _to) public {
+_to.transfer(this.getBalance());
+}
+}
